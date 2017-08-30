bc87942ecc4049209d8963428bf0e112d186fd2a
Joseph Bateh <josephbateh@gmail.com>
2017-08-30 15:37:46 -0400

    Data Binding
    I have the text fields bound to the component's state. Now when the convert button is pressed their values print to the console.

--- client/spm/src/Converter.js
+++ client/spm/src/Converter.js
@@ -3,15 +3,29 @@ import React, {Component} from 'react';
 class Converter extends Component {
     constructor(props) {
         super(props);
-        this.state = {isClicked: true};
-        this.getRequest = this.getRequest.bind(this);
+        this.state = {
+            // number: 0,
+            // base: 10,
+            // newBase: 10
+        };
+        this.convertButtonPressed = this.convertButtonPressed.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
+
+    handleChange(event) {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
     
-    getRequest() {
-        console.log(this.state.isClicked);
-        this.setState(prevState => ({
-            isClicked: !prevState.isClicked
-        }));
+        this.setState({
+          [name]: value
+        });
+      }
+
+    convertButtonPressed(event) {
+        console.log("Number:", this.state.number);
+        console.log("Base:", this.state.base);
+        console.log("New Base:", this.state.newBase);
     }
     
     render() {
@@ -20,13 +34,13 @@ class Converter extends Component {
             <p>Number Converter</p>
             <div>
               Number:
-              <input type="text"></input><br />
+              <input name="number" onChange={this.handleChange} type="text" placeholder="Number"></input><br />
               Base:
-              <input type="text"></input><br />
+              <input name="base" onChange={this.handleChange} type="text" placeholder="Base"></input><br />
               New Base:
-              <input type="text"></input><br />
+              <input name="newBase" onChange={this.handleChange} type="text" placeholder="New Base"></input><br />
             </div>
-            <button onClick={this.getRequest}>Convert</button>
+            <button onClick={this.convertButtonPressed}>Convert</button>
           </div>
         );
       }
