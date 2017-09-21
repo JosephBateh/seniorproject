15bfc069aadea019feb52c1b491cb29dd4e507ee
Joseph Bateh <josephbateh@gmail.com>
2017-09-20 22:10:02 -0400

    Data Between Components
    App is now the truth component for the current state of the app.

--- client/spm/src/playlist/Playlist.js
+++ client/spm/src/playlist/Playlist.js
@@ -1,43 +1,16 @@
 import React, {Component} from 'react';
 import './Playlist.css';
-import axios from 'axios';
 import PlaylistItem from './PlaylistItem';
 import {List, ListItem} from 'material-ui/List';
 
 class Playlist extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
-
-    componentDidMount() {
-        var getURL = 'http://localhost:4000/';
-        if (process.env.REACT_APP_ENV === 'production') {
-            getURL = 'http://dev.josephbateh.com:4000';
-        }
-        axios({
-            method: 'GET',
-            baseURL: getURL,
-            url: '/playlists/0000'
-          })
-          .then((response) => {
-            var responseString = JSON.stringify(response.data);
-            var responseObject = JSON.parse(responseString);
-            this.setState({
-                results: responseObject
-            });            
-          })
-          .catch(function(err) {
-              console.log(err);
-          });
-    }
-    
     render() {
+        const songs = this.props.currentPlaylistSongs;
         return (
             <div className="playlist-wrapper">
                 <div className="playlist">
                     <List className="flex-list">
-                        {this.state.results ? this.state.results.map((result, index) => <PlaylistItem key={index} title={result.Title} artist={result.Artist} album={result.Album}></PlaylistItem>) : <ListItem>Loading...</ListItem>}
+                        {songs ? songs.map((list, index) => <PlaylistItem key={index} title={list.Title} artist={list.Artist} album={list.Album}></PlaylistItem>) : <ListItem>Loading...</ListItem>}
                     </List>
                 </div>
             </div>
--- client/spm/src/sidebar/Sidebar.js
+++ client/spm/src/sidebar/Sidebar.js
@@ -1,43 +1,16 @@
 import React, {Component} from 'react';
 import './Sidebar.css';
 import SidebarItem from './SidebarItem'
-import axios from 'axios';
 import {List, ListItem} from 'material-ui/List';
 
 class Sidebar extends Component {    
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
-
-    componentDidMount() {
-        var getURL = 'http://localhost:4000/';
-        if (process.env.REACT_APP_ENV === 'production') {
-            getURL = 'http://dev.josephbateh.com:4000';
-        }
-        axios({
-            method: 'GET',
-            baseURL: getURL,
-            url: '/playlists'
-          })
-          .then((response) => {
-            var responseString = JSON.stringify(response.data);
-            var responseObject = JSON.parse(responseString);
-            this.setState({
-                results: responseObject
-            });
-          })
-          .catch(function(err) {
-              console.log(err);
-          });
-    }
-    
     render() {
+        const playlists = this.props.userPlaylists;
         return (
             <div className="sidebar-wrapper">
                 <div className="sidebar">
                     <List className="sidebar-flexlist">
-                        {this.state.results ? this.state.results.map((result, index) => <SidebarItem key={index} title={result.Title} creator={result.Creator} ></SidebarItem>) : <ListItem>loading...</ListItem>}
+                        {playlists ? playlists.map((result, index) => <SidebarItem key={index} title={result.Title} creator={result.Creator} ></SidebarItem>) : <ListItem>loading...</ListItem>}
                     </List>
                 </div>
             </div>
