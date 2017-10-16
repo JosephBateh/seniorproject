d6760bd1a048e9f1a698b69ed43a039d0cc9c2f4
Joseph Bateh <josephbateh@gmail.com>
2017-10-16 11:27:40 -0400

    Search Component Update
    Search Component updates when you click search again.

--- client/spm/src/components/search/Search.js
+++ client/spm/src/components/search/Search.js
@@ -1,23 +1,52 @@
 import React, {Component} from 'react';
 import {List} from 'material-ui/List';
-import {Toolbar, ToolbarGroup} from 'material-ui/Toolbar';
-import TextField from 'material-ui/TextField';
-import FlatButton from 'material-ui/FlatButton';
 import ListItem from '../itemlist/ListItem';
+import Searchbar from '../searchbar/Searchbar';
+import * as API from '../../helpers/API.js';
 
 class Search extends Component {
+
+    searchButtonClicked = (text) => {
+        var items = this.props.currentListItems;
+        sessionStorage.setItem('CurrentListItems', JSON.stringify(items));
+
+        // API call fails if text is null or empty
+        if (text) {
+            API.searchSpotify(text).then((data) => {
+                // Parse JSON into my model
+                var x = data.data.tracks.items.map( item => {
+                    x = {
+                        ID: item.id,
+                        Title: item.name,
+                        Artist: item.artists[0].name,
+                        Album: item.album.name
+                    }
+                    return x;
+                });
+                return x;
+            })
+            .then(x => {
+                sessionStorage.setItem('CurrentListItems', JSON.stringify(x));
+                window.location = 'http://localhost:3000/search/';
+            });
+        }
+    }
+
+    onChange = (value) => {
+        sessionStorage.setItem('CurrentSearch', value);
+    }
+
     render() {
         const songs = JSON.parse(sessionStorage.getItem('CurrentListItems'));
         const searchBarText = sessionStorage.getItem('CurrentSearch');
         
         return(
             <div>
-                <Toolbar>
-                    <ToolbarGroup>
-                        <TextField hintText="Search Spotify..." defaultValue={searchBarText || null}/>
-                        <FlatButton label="Go"/>
-                    </ToolbarGroup>
-                </Toolbar>
+                <Searchbar
+                    searchButtonClicked={this.searchButtonClicked}
+                    onTextChange={this.onChange}
+                    text={searchBarText}
+                />
                 <List>
                     {songs ? songs.map((song, index) => <ListItem key={index} title={song.Title} artist={song.Artist} album={song.Album} id={song.ID} onClick={this.onClick}></ListItem>) : <ListItem>Loading...</ListItem>}
                 </List>
