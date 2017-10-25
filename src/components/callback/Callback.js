67becfd31f27facae5d338696ab2c60e20eda47f
Joseph Bateh <josephbateh@gmail.com>
2017-10-25 18:48:01 -0400

    Added notification
    Server auth is working now, but I added a notification on login so users know that they will be logging in twice.

--- src/components/callback/Callback.js
+++ src/components/callback/Callback.js
@@ -4,33 +4,33 @@ import * as API from "../../helpers/API";
 import * as Server from "../../helpers/Server";
 
 class Callback extends Component {
-    state = {
-        userID: null
-    };
+	state = {
+		userID: null
+	};
 
-    componentWillMount() {
-        sessionStorage.clear();
-        var token = window.location.hash.split("=")[1].split("&")[0];
-        API.setToken(token);
+	componentWillMount() {
+		sessionStorage.clear();
+		var token = window.location.hash.split("=")[1].split("&")[0];
+		API.setToken(token);
 
-        Server.serverAuth().then(x => {
-            console.log(x);
-            window.open(x);
-        });
+		Server.serverAuth().then(x => {
+			//console.log(x);
+			window.open(x);
+		});
 
-        // The code block below attempts to force userID
-        // to be set before moving to the app
-        API.getUser(token).then(ID => {
-            API.setUserID(ID);
-            this.setState({
-                userID: ID
-            });
-        });
-    }
+		// The code block below attempts to force userID
+		// to be set before moving to the app
+		API.getUser(token).then(ID => {
+			API.setUserID(ID);
+			this.setState({
+				userID: ID
+			});
+		});
+	}
 
-    render() {
-        return this.state.userID ? <Redirect to="/playlist" /> : <div>Loading...</div>;
-    }
+	render() {
+		return this.state.userID ? <Redirect to="/playlist" /> : <div>Loading...</div>;
+	}
 }
 
 export default Callback;
