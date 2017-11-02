e11e1f650d124d61fee15a5bf52bfcfb433eb4d6
Joseph Bateh <josephbateh@gmail.com>
2017-11-02 16:01:26 -0400

    Add Buttons
    Currently I have add buttons for a playlist and smart playlist. I'm not quite content with how they look in terms of the UI, but for now they will do.

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -2,34 +2,42 @@ import React, { Component } from "react";
 import SidebarItem from "./SidebarItem";
 import Drawer from "material-ui/Drawer";
 import { List, ListItem } from "material-ui/List";
+import FlatButton from "material-ui/FlatButton";
+import SmartAdd from "material-ui/svg-icons/av/playlist-add";
+import AddButton from "material-ui/svg-icons/content/add-circle-outline";
+import { BottomNavigation, BottomNavigationItem } from "material-ui/BottomNavigation";
 
 class Sidebar extends Component {
-    onClick = value => {
-        this.props.onClick(value);
-    };
+	onClick = value => {
+		this.props.onClick(value);
+	};
 
-    render() {
-        const playlists = this.props.playlists;
-        return (
-            <Drawer className="sidebar" open={true}>
-                <List className="sidebar-flexlist">
-                    {playlists ? (
-                        playlists.map((playlist, index) => (
-                            <SidebarItem
-                                id={playlist.UUID}
-                                onClick={this.onClick}
-                                key={index}
-                                title={playlist.Title}
-                                creator={playlist.Creator}
-                            />
-                        ))
-                    ) : (
-                        <ListItem>loading...</ListItem>
-                    )}
-                </List>
-            </Drawer>
-        );
-    }
+	render() {
+		const playlists = this.props.playlists;
+		return (
+			<Drawer className="sidebar" open={true}>
+				<BottomNavigation>
+					<BottomNavigationItem label="Playlist" icon={<AddButton />} />
+					<BottomNavigationItem label="Smart Playlist" icon={<SmartAdd />} />
+				</BottomNavigation>
+				<List className="sidebar-flexlist">
+					{playlists ? (
+						playlists.map((playlist, index) => (
+							<SidebarItem
+								id={playlist.UUID}
+								onClick={this.onClick}
+								key={index}
+								title={playlist.Title}
+								creator={playlist.Creator}
+							/>
+						))
+					) : (
+						<ListItem>loading...</ListItem>
+					)}
+				</List>
+			</Drawer>
+		);
+	}
 }
 
 export default Sidebar;
--- src/components/sidebar/SidebarItem.js
+++ src/components/sidebar/SidebarItem.js
@@ -2,19 +2,19 @@ import React, { Component } from "react";
 import { ListItem } from "material-ui/List";
 
 class SidebarItem extends Component {
-    onClick = () => {
-        this.props.onClick(this.props.id);
-    };
+	onClick = () => {
+		this.props.onClick(this.props.id);
+	};
 
-    render() {
-        return (
-            <ListItem
-                onClick={this.onClick}
-                className="sidebar-item"
-                primaryText={<p>{this.props.title}</p>}
-            />
-        );
-    }
+	render() {
+		return (
+			<ListItem
+				onClick={this.onClick}
+				className="sidebar-item"
+				primaryText={<p>{this.props.title}</p>}
+			/>
+		);
+	}
 }
 
 export default SidebarItem;
