4faa323196bdc8da827bb59bef4edafc467528f4
Joseph Bateh <josephbateh@gmail.com>
2017-11-03 11:41:22 -0400

    N Rules Based off of State
    The smart playlist view will show the number of rules based off of state

--- src/components/smartplaylist/Rule.js
+++ src/components/smartplaylist/Rule.js
@@ -9,45 +9,45 @@ import Remove from "material-ui/svg-icons/content/remove";
 import Loading from "../loading/Loading";
 
 class Rule extends Component {
-	render() {
-		const playlists = this.props.playlists;
-		return playlists ? (
-			<div>
-				<Divider />
-				<Toolbar style={{ backgroundColor: "white" }}>
-					<ToolbarGroup firstChild={true}>
-						<DropDownMenu value={1}>
-							<MenuItem value={1} primaryText="Playlist" />
-						</DropDownMenu>
-						<DropDownMenu value={1}>
-							<MenuItem value={1} primaryText="is" />
-							<MenuItem value={2} primaryText="is not" />
-						</DropDownMenu>
-						<DropDownMenu value={1}>
-							{playlists.map((playlist, index) => {
-								return (
-									<MenuItem
-										key={index}
-										value={index}
-										primaryText={playlist.Title}
-									/>
-								);
-							})}
-						</DropDownMenu>
-					</ToolbarGroup>
-					<ToolbarGroup lastChild={true}>
-						<div>
-							<FlatButton icon={<Remove />} />
-							<FlatButton icon={<Add />} />
-						</div>
-					</ToolbarGroup>
-				</Toolbar>
-				<Divider />
-			</div>
-		) : (
-			<Loading />
-		);
-	}
+    render() {
+        const playlists = this.props.playlists;
+        return playlists ? (
+            <div>
+                <Divider />
+                <Toolbar style={{ backgroundColor: "white" }}>
+                    <ToolbarGroup firstChild={true}>
+                        <DropDownMenu value={0}>
+                            <MenuItem value={0} primaryText="Playlist" />
+                        </DropDownMenu>
+                        <DropDownMenu value={0}>
+                            <MenuItem value={0} primaryText="is" />
+                            <MenuItem value={1} primaryText="is not" />
+                        </DropDownMenu>
+                        <DropDownMenu value={0}>
+                            {playlists.map((playlist, index) => {
+                                return (
+                                    <MenuItem
+                                        key={index}
+                                        value={index}
+                                        primaryText={playlist.Title}
+                                    />
+                                );
+                            })}
+                        </DropDownMenu>
+                    </ToolbarGroup>
+                    <ToolbarGroup lastChild={true}>
+                        <div>
+                            <FlatButton icon={<Remove />} />
+                            <FlatButton icon={<Add />} />
+                        </div>
+                    </ToolbarGroup>
+                </Toolbar>
+                <Divider />
+            </div>
+        ) : (
+            <Loading />
+        );
+    }
 }
 
 export default Rule;
--- src/components/smartplaylist/SmartPlaylist.js
+++ src/components/smartplaylist/SmartPlaylist.js
@@ -1,21 +1,32 @@
 import React, { Component } from "react";
 import Rule from "./Rule";
-import Loading from "../loading/Loading";
 
 class SmartPlaylist extends Component {
-	state = {
-		rules: []
-	};
+    state = {
+        rules: [
+            {
+                attribute: "playlist",
+                is: true,
+                value: "Maroon 5"
+            }
+        ]
+    };
 
-	render() {
-		return this.state.rules ? (
-			<div style={{ margin: 15 }}>
-				<Rule playlists={this.props.playlists} />
-			</div>
-		) : (
-			<Loading />
-		);
-	}
+    render() {
+        return (
+            <div style={{ margin: 15 }}>
+                {this.state.rules.map((rule, index) => (
+                    <Rule
+                        key={index}
+                        attribute={rule.attribute}
+                        is={rule.is}
+                        value={rule.value}
+                        playlists={this.props.playlists}
+                    />
+                ))}
+            </div>
+        );
+    }
 }
 
 export default SmartPlaylist;
