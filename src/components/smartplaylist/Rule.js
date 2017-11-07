8fb2a98003dc1adc8cab27dd9e6e58fa2fcdad38
Joseph Bateh <josephbateh@gmail.com>
2017-11-07 10:24:02 -0500

    Rule State
    Rules now have state.

--- src/components/smartplaylist/Rule.js
+++ src/components/smartplaylist/Rule.js
@@ -9,52 +9,61 @@ import Remove from "material-ui/svg-icons/content/remove";
 import Loading from "../loading/Loading";
 
 class Rule extends Component {
-    addRule = () => {
-        this.props.addRule();
-    };
-    deleteRule = () => {
-        this.props.deleteRule(this.props.index);
-    };
+	addRule = () => {
+		this.props.addRule();
+	};
 
-    render() {
-        const playlists = this.props.playlists;
-        return playlists ? (
-            <div>
-                <Divider />
-                <Toolbar style={{ backgroundColor: "white" }}>
-                    <ToolbarGroup firstChild={true}>
-                        <DropDownMenu value={0}>
-                            <MenuItem value={0} primaryText="Playlist" />
-                        </DropDownMenu>
-                        <DropDownMenu value={0}>
-                            <MenuItem value={0} primaryText="is" />
-                            <MenuItem value={1} primaryText="is not" />
-                        </DropDownMenu>
-                        <DropDownMenu value={0}>
-                            {playlists.map((playlist, index) => {
-                                return (
-                                    <MenuItem
-                                        key={index}
-                                        value={index}
-                                        primaryText={playlist.Title}
-                                    />
-                                );
-                            })}
-                        </DropDownMenu>
-                    </ToolbarGroup>
-                    <ToolbarGroup lastChild={true}>
-                        <div>
-                            <FlatButton onClick={this.deleteRule} icon={<Remove />} />
-                            <FlatButton onClick={this.addRule} icon={<Add />} />
-                        </div>
-                    </ToolbarGroup>
-                </Toolbar>
-                <Divider />
-            </div>
-        ) : (
-            <Loading />
-        );
-    }
+	deleteRule = () => {
+		this.props.deleteRule(this.props.index);
+	};
+
+	changeMatch = (event, key, value) => {
+		this.props.changeMatch(this.props.index, key);
+	};
+
+	changeValue = (event, key, value) => {
+		this.props.changeValue(this.props.index, key);
+	};
+
+	render() {
+		const playlists = this.props.playlists;
+		return playlists ? (
+			<div>
+				<Divider />
+				<Toolbar style={{ backgroundColor: "white" }}>
+					<ToolbarGroup firstChild={true}>
+						<DropDownMenu value={0}>
+							<MenuItem value={0} primaryText="Playlist" />
+						</DropDownMenu>
+						<DropDownMenu value={this.props.match} onChange={this.changeMatch}>
+							<MenuItem value={0} primaryText="is" />
+							<MenuItem value={1} primaryText="is not" />
+						</DropDownMenu>
+						<DropDownMenu value={this.props.value} onChange={this.changeValue}>
+							{playlists.map((playlist, index) => {
+								return (
+									<MenuItem
+										key={index}
+										value={index}
+										primaryText={playlist.Title}
+									/>
+								);
+							})}
+						</DropDownMenu>
+					</ToolbarGroup>
+					<ToolbarGroup lastChild={true}>
+						<div>
+							<FlatButton onClick={this.deleteRule} icon={<Remove />} />
+							<FlatButton onClick={this.addRule} icon={<Add />} />
+						</div>
+					</ToolbarGroup>
+				</Toolbar>
+				<Divider />
+			</div>
+		) : (
+			<Loading />
+		);
+	}
 }
 
 export default Rule;
--- src/components/smartplaylist/SmartPlaylist.js
+++ src/components/smartplaylist/SmartPlaylist.js
@@ -2,55 +2,85 @@ import React, { Component } from "react";
 import Rule from "./Rule";
 
 class SmartPlaylist extends Component {
-    state = {
-        rules: [
-            {
-                attribute: "playlist",
-                is: true,
-                value: "Maroon 5"
-            }
-        ]
-    };
+	state = {
+		rules: [
+			{
+				attribute: "playlist",
+				matches: [true, false],
+				values: this.props.playlists,
+				state: { match: 0, value: 0 }
+			}
+		]
+	};
 
-    addRule = () => {
-        var newRules = this.state.rules;
-        newRules.push({
-            attribute: "playlist",
-            is: true,
-            value: "Maroon 5"
-        });
-        this.setState({
-            rules: newRules
-        });
-    };
+	addRule = () => {
+		var newRules = this.state.rules;
+		newRules.push({
+			attribute: "playlist",
+			matches: [true, false],
+			values: this.props.playlists,
+			state: { match: 0, value: 0 }
+		});
+		this.setState({
+			rules: newRules
+		});
+	};
 
-    deleteRule = rule => {
-        var newRules = this.state.rules;
-        newRules.splice(3, 1);
-        this.setState({
-            rules: newRules
-        });
-    };
+	deleteRule = rule => {
+		var newRules = this.state.rules;
+		if (newRules.length > 1) {
+			newRules.splice(rule, 1);
+		}
+		this.setState({
+			rules: newRules
+		});
+	};
 
-    render() {
-        const rules = this.state.rules;
-        return (
-            <div style={{ margin: 15 }}>
-                {rules.map((rule, index) => (
-                    <Rule
-                        key={index}
-                        index={index}
-                        attribute={rule.attribute}
-                        is={rule.is}
-                        value={rule.value}
-                        playlists={this.props.playlists}
-                        addRule={this.addRule}
-                        deleteRule={this.deleteRule}
-                    />
-                ))}
-            </div>
-        );
-    }
+	changeMatch = (rule, value) => {
+		var rules = this.state.rules;
+		var newRule = rules[rule];
+		var newState = newRule.state;
+		newState.match = value;
+		newRule.state = newState;
+		rules.splice(rule, 1, newRule);
+		this.setState({
+			rules: rules
+		});
+	};
+
+	changeValue = (rule, value) => {
+		var rules = this.state.rules;
+		var newRule = rules[rule];
+		var newState = newRule.state;
+		newState.value = value;
+		newRule.state = newState;
+		rules.splice(rule, 1, newRule);
+		this.setState({
+			rules: rules
+		});
+	};
+
+	render() {
+		const rules = this.state.rules;
+		return (
+			<div style={{ margin: 15 }}>
+				{rules.map((rule, index) => (
+					<Rule
+						key={index}
+						index={index}
+						attribute={rule.attribute}
+						match={rule.state.match}
+						value={rule.state.value}
+						playlists={this.props.playlists}
+						addRule={this.addRule}
+						deleteRule={this.deleteRule}
+						changeMatch={this.changeMatch}
+						changeValue={this.changeValue}
+					/>
+				))}
+			</div>
+		);
+	}
 }
 
 export default SmartPlaylist;
